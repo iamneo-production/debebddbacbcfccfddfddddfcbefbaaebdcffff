87280a16612305487ee532c71daf1c94758ce19a
Iamneo <techadmin@iamneo.ai>
2023-08-08 15:14:59 +0000

    Backup Commit

--- .vscode/testng1/test/src/test/java/test/AppTest.java
+++ .vscode/testng1/test/src/test/java/test/AppTest.java
@@ -1,20 +1,56 @@
 package test;
 
-import static org.junit.Assert.assertTrue;
+import org.testng.annotations.Test;
+import java.net.URL;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 
-import org.junit.Test;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	ChromeOptions chromeOptions = new ChromeOptions();
+	WebDriver driver = null;
+
+
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		
+		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+        driver.get("https://flipkart.com/");
+
+	}
+
+	@Test
+	public void testcase_1() throws InterruptedException //Go to Ebay
+	{
+		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+	    int count= allLinks.size();
+
+	}
+
+	@Test
+	public void testcase_2() throws InterruptedException 
+      {
+		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+		for (WebElement link : allLinks) 
+		{
+			System.out.println("Link Text: " + link.getText() + " Link URL: " + link.getAttribute("href"));
+		}
+		
+	}
+	
+	     
+		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+
 }
+
